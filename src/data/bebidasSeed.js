d5ff3cab83e74daacc629808547a690e0b7abd87
kacn1z-del <kacn1z@gmail.com>
2026-09-12 15:23:25 -0600

    Create bebidasSeed.js

---
+++ src/data/bebidasSeed.js
@@ -0,0 +1,67 @@
+// Cervezas, licores, helados, gaseosas, jugos y smoothies, transcritos del
+// sistema de punto de venta viejo que usaba Los Pirchas.
+
+export const BEBIDAS_SEED = [
+  // Cerveza Internacional
+  { nombre: 'Leffe Blonde', categoria: 'Cerveza Internacional', precio: 2500 },
+  { nombre: 'Leffe Brune', categoria: 'Cerveza Internacional', precio: 2500 },
+  { nombre: 'Lindemans Kriek', categoria: 'Cerveza Internacional', precio: 3000 },
+  { nombre: 'Hoegaarden', categoria: 'Cerveza Internacional', precio: 2500 },
+  { nombre: 'Estrella', categoria: 'Cerveza Internacional', precio: 2500 },
+  { nombre: 'Stella Artois', categoria: 'Cerveza Internacional', precio: 2500 },
+  { nombre: 'Carlsberg', categoria: 'Cerveza Internacional', precio: 2500 },
+  { nombre: 'Duvel', categoria: 'Cerveza Internacional', precio: 3500 },
+  { nombre: 'Blanc', categoria: 'Cerveza Internacional', precio: 2500 },
+  { nombre: 'Corona', categoria: 'Cerveza Internacional', precio: 2200 },
+  { nombre: 'Sol', categoria: 'Cerveza Internacional', precio: 2200 },
+  { nombre: 'Bavaria Gold', categoria: 'Cerveza Internacional', precio: 2200 },
+  { nombre: 'Bavaria Light', categoria: 'Cerveza Internacional', precio: 2200 },
+  { nombre: 'Bavaria Masters', categoria: 'Cerveza Internacional', precio: 2200 },
+  { nombre: 'Heineken', categoria: 'Cerveza Internacional', precio: 2200 },
+
+  // Cerveza Nacional
+  { nombre: 'Imperial Light', categoria: 'Cerveza Nacional', precio: 1600 },
+  { nombre: 'Imperial Silver', categoria: 'Cerveza Nacional', precio: 1600 },
+  { nombre: 'Imperial Ultra', categoria: 'Cerveza Nacional', precio: 1600 },
+  { nombre: 'Pilsen', categoria: 'Cerveza Nacional', precio: 1600 },
+
+  // Licores
+  { nombre: 'Vino copa', categoria: 'Licores', precio: 3900 },
+  { nombre: 'Smirnoff Ice manzana verde', categoria: 'Licores', precio: 2700 },
+  { nombre: 'Smirnoff Ice negra', categoria: 'Licores', precio: 2700 },
+  { nombre: 'Smirnoff Ice roja', categoria: 'Licores', precio: 2700 },
+  { nombre: 'Smirnoff Ice guaraná', categoria: 'Licores', precio: 2700 },
+  { nombre: 'Adán y Eva', categoria: 'Licores', precio: 2000 },
+
+  // Helados
+  { nombre: 'Cono', categoria: 'Helados', precio: 1000 },
+  { nombre: 'Copa de helado', categoria: 'Helados', precio: 2200 },
+  { nombre: 'Topping', categoria: 'Helados', precio: 400 },
+  { nombre: 'Horchata (helado)', categoria: 'Helados', precio: 2200 },
+
+  // Gaseosas y refrescos
+  { nombre: 'Coca Cola', categoria: 'Gaseosas y refrescos', precio: 1450 },
+  { nombre: 'Coca Cola Zero', categoria: 'Gaseosas y refrescos', precio: 1450 },
+  { nombre: 'Fanta Naranja', categoria: 'Gaseosas y refrescos', precio: 1450 },
+  { nombre: 'Fanta Kolita', categoria: 'Gaseosas y refrescos', precio: 1450 },
+  { nombre: 'Fanta Uva', categoria: 'Gaseosas y refrescos', precio: 1450 },
+  { nombre: 'Ginger Ale', categoria: 'Gaseosas y refrescos', precio: 1450 },
+  { nombre: 'Soda', categoria: 'Gaseosas y refrescos', precio: 1450 },
+  { nombre: 'Tropical Arándano', categoria: 'Gaseosas y refrescos', precio: 1450 },
+  { nombre: 'Tropical Frutas', categoria: 'Gaseosas y refrescos', precio: 1450 },
+  { nombre: 'Tropical Cero', categoria: 'Gaseosas y refrescos', precio: 1450 },
+  { nombre: 'Tropical Melocotón', categoria: 'Gaseosas y refrescos', precio: 1450 },
+
+  // Jugos
+  { nombre: 'Jugo de naranja', categoria: 'Jugos', precio: 2200 },
+  { nombre: 'Jugo de tomate', categoria: 'Jugos', precio: 2200 },
+
+  // Smoothies
+  { nombre: 'Smoothie de chocolate', categoria: 'Smoothies', precio: 2200 },
+  { nombre: 'Smoothie fresa hierbabuena', categoria: 'Smoothies', precio: 2200 },
+  { nombre: 'Smoothie fresa mango', categoria: 'Smoothies', precio: 2200 },
+  { nombre: 'Smoothie fresa guanábana', categoria: 'Smoothies', precio: 2800 },
+  { nombre: 'Smoothie piña mango', categoria: 'Smoothies', precio: 2200 },
+  { nombre: 'Smoothie piña hierbabuena', categoria: 'Smoothies', precio: 2200 },
+  { nombre: 'Mocachino', categoria: 'Smoothies', precio: 2000 },
+]
